f6753df3f133fe8afc63b13574e0742e7bef2461
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-10 19:28:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const partag = document.getElementById("metaverse");
+const versebtn = document.getElementById("enterBtn");
+
+versebtn.addEventListener("click",()=>{
+  const newText = partag.textContent;
+
+  partag.innerHTML = `<h1>${newText}</h1>`;
+});
